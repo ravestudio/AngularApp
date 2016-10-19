9b02754b6e4901dbee3590e94f79abe9ede4cb50
ravestudio <ravestudio@mail.ru>
2016-10-19 14:27:33 +0300

    project created

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult SimpleApp()
+        {
+            return View();
+        }
+
+        public ActionResult GreetingController()
+        {
+            return View();
+        }
+
+        public ActionResult TwoWayBinding()
+        {
+            return View();
+        }
+
+        public ActionResult Blogger()
+        {
+            return View();
+        }
+
+        public ActionResult ApplyInAction()
+        {
+            return View();
+        }
+
+        public ActionResult EventsInAction()
+        {
+            return View();
+        }
+
+        public ActionResult MultipleViews()
+        {
+            return View();
+        }
+
+        public ActionResult UIRoutting()
+        {
+            return View();
+        }
+
+        public ActionResult Validation()
+        {
+            return View();
+        }
+    }
+}
---
+++ Scripts/Blogger/app.js
@@ -0,0 +1,7 @@
+﻿
+angular.module('myApp', ['myApp.controllers']);
+
+angular.module('myApp').run(function($rootScope){
+    $rootScope.title='Famous Books';
+    $rootScope.name="Root Scope";
+});
